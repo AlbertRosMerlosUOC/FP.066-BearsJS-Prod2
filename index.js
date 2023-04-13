8e27a251c2486c02120f68118b871c52d62ef31c
Marcus <crocushtml@gmail.com>
2023-04-13 18:39:16 +0200

    some adds graphql y app-folder

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = {
+  hello: () => 'Hello World!'
+};
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola, mundo!');
-});
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: rootValue,
+  graphiql: true
+}));
 
 app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+  console.log('Server started on port 3000');
 });
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/BearsJS-Prod2', { useNewUrlParser: true, useUnifiedTopology: true });
